985b5ab26ef2a6467b5a505fff530ce488ab2305
sahas-arora <sahasarora@gmail.com>
2021-03-10 15:11:00 +0100

    IndexScreen added

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const IndexScreen = () => {
+
+    return(
+        <View>
+            <Text>Index Screen!</Text>
+        </View>
+    );
+}
+
+export default IndexScreen;
